2ff5fc13ee4f33d6dd4da46fc18e8988d8c4aa03
Colm Cahalane <me@colm.cf>
2016-09-05 12:13:18 +0100

    Create impressive-cli

---
+++ impressive-cli.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/colm2/impressive"
+	"log"
+	"os"
+)
+
+const (
+	usage    = "usage: impressive email@umail.ucc.ie password"
+	errormsg = "An error occured genrating your calendar. Did you enter your username and password?"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatal(usage)
+	}
+	ical, err := impressive.GetICal(os.Args[1], os.Args[2])
+	if err != nil {
+		log.Fatal(errormsg + "\n" + usage)
+	}
+
+	log.Print(ical)
+}
